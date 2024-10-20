2b4aa8cbff33a0aa53c1faf4fe18b57aae0d8c08
k3yJ3y <klemen.jerman@outlook.com>
2024-10-20 17:51:49 +0200

    added authentication

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/upload-image', [ImageController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/upload-image', [ImageController::class, 'store']);
+});
